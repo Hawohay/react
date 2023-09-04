b5957fa18e576bdda9be5185365ae90cbe7aaed7
hawohay <hawohay@outlook.com>
2023-09-04 16:47:39 -0300

    feat: Import de ícones do MUI removido

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter } from "react-router-dom";
 
 import { AppRoutes } from "./routes";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
-import { LightMode } from "@mui/icons-material";
 import { LightTheme } from "./shared/themes";
 
 export const App = () => {
