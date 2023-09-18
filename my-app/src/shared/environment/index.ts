afdb146a69e380df54189e8da4720954b8c5cecc
hawohay <hawohay@outlook.com>
2023-09-18 11:37:39 -0300

    feat: environment

--- my-app/src/shared/environment/index.ts
+++ my-app/src/shared/environment/index.ts
@@ -1,1 +1,21 @@
-export {};
+
+
+export const Environment = {
+    /**
+     * Define a quantidade de linhas a serem carregadas por padrão nas listagens
+     */
+    LIMITE_DE_LINHAS: 10,
+    /**
+     * Placeholder exibidos nas consultas
+     */
+    INPUT_DE_BUSCA: 'Pesquisar...',
+    /**
+     * Texto exibido quando nenhum registro é encontrado em uma listagem
+     */
+    LISTAGEM_VAZIA: 'Nenhum registro encontrado.',
+    /**
+     * Url base de consulta dos dados da aplicação
+     */
+    URL_BASE: 'http://localhost:3333',
+};
+
